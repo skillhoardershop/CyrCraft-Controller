70ac7ba06f87884880c52cde893bd4e75615c018
Kevin Miller <kevinamiller77@gmail.com>
2019-06-01 22:15:46 -0400

    Required library files

---
+++ Release/SD/sd_diskio_crc.c.d
@@ -0,0 +1,2 @@
+C:\Users\there\AppData\Local\Temp\VMBuilds\SD_To_LED\esp32_esp32\Release\SD\sd_diskio_crc.c.o: \
+ C:\Users\there\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.2\libraries\SD\src\sd_diskio_crc.c
